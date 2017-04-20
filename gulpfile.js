712bdecb7da64565acce30cb7b68950c22a414b1
Runar Brastad <runar.brastad@gmail.com>
2017-04-20 14:36:08 +0200

    FIX check if bintray user and apikey is in prop file.

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,26 @@
 // Folder variables
 var srcSite = 'src/main/resources';
 var buildSite = 'build/resources/main';
+var buildSiteJSX = 'build/resources/main/**/*.jsx';
+var buildSiteES6 = 'build/resources/main/**/*.es6';
+var buildSiteAppJsx = 'build/resources/main/assets/app';
+var buildSiteAppScss = 'build/resources/main/assets/scss';
+var buildAssets = buildSite + '/assets/dist';
+
+
 var srcAssetsApp = srcSite + '/assets/**/*';
 var srcES6 = srcSite + '/**/*.es6';
-var buildAssets = buildSite + '/assets/dist';
 
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var gulpWebpack = require('webpack-stream');
 var prettyError = require('gulp-prettyerror');
+var del = require('del');
 
+gulp.task('default',['babel', 'webpack','buildDoneWebpackClean']);
 
-gulp.task('default',['babel', 'webpack']);
-
-gulp.task('webpack', function() {
+gulp.task('webpack',['webpack-prod','buildDoneES6Clean','buildDoneWebpackClean']);
+gulp.task('webpack-prod', function() {
     return gulp
         .src( srcAssetsApp )
         .pipe(prettyError())
@@ -24,8 +31,9 @@ gulp.task('webpack', function() {
         .pipe(gulp.dest(buildAssets));
 });
 
-
-gulp.task('webpack-dev', function() {
+// Do it like this so we can add more tasks easy without breaking stuff
+gulp.task('webpack-dev',['webpack-dev-jsxsccss']);
+gulp.task('webpack-dev-jsxsccss', function() {
     return gulp
         .src( srcAssetsApp )
         .pipe(prettyError())
@@ -33,8 +41,9 @@ gulp.task('webpack-dev', function() {
         .pipe(gulp.dest(buildAssets));
 });
 
-
-gulp.task('babel', () => {
+// Do it like this so we can add more tasks easy without breaking stuff
+gulp.task('babel',['babelES6']);
+gulp.task('babelES6', () => {
     return gulp.src( srcES6 )
         .pipe(prettyError())
         .pipe(babel({
@@ -43,6 +52,21 @@ gulp.task('babel', () => {
             ]
         }))
         .pipe(gulp.dest( buildSite ));
+});
+
+// Do it like this so we can add more tasks easy without breaking stuff
+gulp.task('buildDoneClean',['buildDoneES6Clean','buildDoneWebpackClean']);
+gulp.task('buildDoneES6Clean', function () {
+    return del([
+        buildSiteES6
+    ]);
+});
+
+gulp.task('buildDoneWebpackClean', function () {
+    return del([
+        buildSiteAppJsx,
+        buildSiteAppScss
+    ]);
 });
 
 gulp.task('watch', function() {
